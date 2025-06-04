2611051e88309a47544345e62728ea31257e9f16
Dandingo <dedominguez97@gmail.com>
2025-06-04 12:04:11 -0400

    Update html tags in the app component and changed nav-bar component to use material library

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 @Component({
 	selector: 'nav-bar',
 	standalone: true,
-	imports: [MatButtonModule, RouterLink, RouterLinkActive],
+	imports: [MatButtonModule, MatListModule, RouterLink, RouterLinkActive],
 	templateUrl: './nav-bar.component.html',
 	styleUrl: './nav-bar.component.css',
 })
